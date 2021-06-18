d8925d3be8432454809c5505023a01ba94790f74
Ghostlun <40644178+Ghostlun@users.noreply.github.com>
2021-06-18 00:29:40 -0500

    first commit

---
+++ Soliu/Soliu/Modules/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Soliu
+//
+//  Created by Yoonha Kim on 6/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
