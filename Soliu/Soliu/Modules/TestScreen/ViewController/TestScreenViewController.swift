fb5a313d974e8ca528c603c617ce01e8775e87ea
Jungpyo Hong <54448459+jphong1111@users.noreply.github.com>
2021-08-28 01:04:23 -0500

    edit test tableview

--- Soliu/Soliu/Modules/TestScreen/ViewController/TestScreenViewController.swift
+++ Soliu/Soliu/Modules/TestScreen/ViewController/TestScreenViewController.swift
@@ -22,6 +22,7 @@ class TestScreenViewController: UIViewController {
     lazy var viewModel = TestScreenViewModel(delegate: self)
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewModel.populateDataFromJson()
     }
 }
 
