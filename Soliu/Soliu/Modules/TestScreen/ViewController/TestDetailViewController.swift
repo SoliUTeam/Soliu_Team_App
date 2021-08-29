81c46b4ed14b11d42f782c889f698c14d6e45af2
Jungpyo Hong <54448459+jphong1111@users.noreply.github.com>
2021-08-29 01:37:18 -0500

    feature[TestScreen]: edit test detail screen & setup function structure

--- Soliu/Soliu/Modules/TestScreen/ViewController/TestDetailViewController.swift
+++ Soliu/Soliu/Modules/TestScreen/ViewController/TestDetailViewController.swift
@@ -10,12 +10,56 @@ import UIKit
 
 class TestDetailViewController: UIViewController {
     
+    @IBOutlet private weak var question: UILabel!
+    @IBOutlet private weak var answer1: UIButton!
+    @IBOutlet private weak var answer2: UIButton!
+    @IBOutlet private weak var answer3: UIButton!
+    @IBOutlet private weak var answer4: UIButton!
+    @IBOutlet private weak var answer5: UIButton!
+    @IBOutlet private weak var progressView: UIProgressView!
+    @IBOutlet private weak var currentQuestion: UILabel!
+    @IBOutlet private weak var totalQuestion: UILabel!
+    
     
     var questionDataSource: TestSection?
+    var currentQuestionNumber = 1
+    
+    lazy var viewModel = TestDetailViewModel()
     
     override func viewDidLoad() {
-        self.viewDidLoad()
+        super.viewDidLoad()
+        currentQuestionNumber = 1
     }
     
+    // upload data to firebase
+    @IBAction private func touchAnswer1() {
+        increaseProgress()
+        viewModel.uploadData()
+    }
+    
+    @IBAction private func touchAnswer2() {
+        increaseProgress()
+        viewModel.uploadData()
+    }
+    
+    @IBAction private func touchAnswer3() {
+        increaseProgress()
+        viewModel.uploadData()
+    }
+    
+    @IBAction private func touchAnswer4() {
+        increaseProgress()
+        viewModel.uploadData()
+    }
+    
+    @IBAction private func touchAnswer5() {
+        increaseProgress()
+        viewModel.uploadData()
+    }
+    
+    private func increaseProgress() {
+        progressView.progress = Float(currentQuestionNumber / questionDataSource!.testNumber)
+        currentQuestionNumber += 1
+    }
     
 }
---
+++ Soliu/Soliu/Modules/TestScreen/ViewModel/TestDetailViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  TestDetailViewModel.swift
+//  Soliu
+//
+//  Created by JungpyoHong on 8/28/21.
+//
+import Firebase
+import Foundation
+
+class TestDetailViewModel {
+    
+    // upload data to firebase
+    func uploadData() {
+        print("uploaded successful!!")
+    }
+}
