daa67efe7f5f51bc23639db59f89d1fdfb329282
Ghostlun <40644178+Ghostlun@users.noreply.github.com>
2021-09-07 20:31:54 -0500

    test Commits
    test Commits

--- Soliu/Soliu/Modules/ProfileScreen/ProfileViewController.swift
+++ Soliu/Soliu/Modules/ProfileScreen/ProfileViewController.swift
@@ -11,7 +11,7 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("Profile View Controller Test")
         // Do any additional setup after loading the view.
     }
 
