c86f5c60ff0c4a3f716d8b071e46f67317bbd3bc
Jungpyo Hong <54448459+jphong1111@users.noreply.github.com>
2021-09-17 09:03:48 -0500

    feature[Loading Spinner]: add loading spinner json file

---
+++ Soliu/Soliu/Modules/ResusableComponents/LoadingSpinner.swift
@@ -0,0 +1,16 @@
+//
+//  LoadingSpinner.swift
+//  Soliu
+//
+//  Created by JungpyoHong on 9/16/21.
+//
+
+import Lottie
+
+protocol LoadingSpinnerProtocol {
+    
+}
+
+class LoadingSpinner {
+    
+}
